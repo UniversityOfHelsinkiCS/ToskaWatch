d57c2c9ccfe1aa8f27472181797e05cde4f928a6
Mäkinen Sasu S <sasu.makinen@helsinki.fi>
2019-03-25 16:41:03 +0200

    token properly set

--- src/release.sh
+++ src/release.sh
@@ -4,13 +4,12 @@ body="{
 \"request\": {
   \"stage\":\"staging\"
 }}"
-
+TOKEN=$1
 curl -s -X POST \
   -H "Content-Type: application/json" \
   -H "Accept: application/json" \
-  -H "Authorization: token ${process.env.TRAVIS_ACCESS_TOKEN}" \
+  -H "Authorization: token ${TOKEN}" \
   -d "$body" \
   https://oodikone.cs.helsinki.fi/release/ \
  | tee /tmp/release-request-output.$$.txt
 
- cat /tmp/release-request-output.$$.txt
