dfc307786b4eb463ceb46b6c1f2fbb7dcff7fff8
Mäkinen Sasu S <sasu.makinen@helsinki.fi>
2019-03-25 16:27:30 +0200

    echo -> cat and travis access token

--- src/release.sh
+++ src/release.sh
@@ -8,9 +8,9 @@ body="{
 curl -s -X POST \
   -H "Content-Type: application/json" \
   -H "Accept: application/json" \
-  -H "Authorization: token ${TOKEN}" \
+  -H "Authorization: token ${process.env.TRAVIS_ACCESS_TOKEN}" \
   -d "$body" \
   https://oodikone.cs.helsinki.fi/release/ \
  | tee /tmp/release-request-output.$$.txt
 
-echo /tmp/release-request-output.$$.txt
+ cat /tmp/release-request-output.$$.txt
