2daf68fac3171d340cd91d5605f5d082a866c790
Mäkinen Sasu S <sasu.makinen@helsinki.fi>
2019-03-25 15:53:01 +0200

    deploy script

---
+++ src/release.sh
@@ -0,0 +1,12 @@
+body="{
+\"request\": {
+  \"stage\":\"staging\"
+}}"
+
+curl -s -X POST \
+  -H "Content-Type: application/json" \
+  -H "Accept: application/json" \
+  -H "Authorization: token ${TOKEN}" \
+  -d "$body" \
+  https://oodikone.cs.helsinki.fi/release/ \
+ | tee /tmp/release-request-output.$$.txt
