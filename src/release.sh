72f297d9026923108021a9406efb168d651effa0
Mäkinen Sasu S <sasu.makinen@helsinki.fi>
2019-03-26 11:46:56 +0200

    -k

--- src/release.sh
+++ src/release.sh
@@ -6,12 +6,9 @@ body="{
 }}"
 TOKEN=$1
 PANNU=$2
-curl -s -X POST \
+curl -k -X POST \
   -H "Content-Type: application/json" \
   -H "Accept: application/json" \
   -H "Authorization: token ${TOKEN}" \
   -d "$body" \
-  https://${PANNU}.cs.helsinki.fi/release/ \
- | tee /tmp/release-request-output.$$.txt
-
-cat /tmp/release-request-output.$$.txt
+  https://${PANNU}.cs.helsinki.fi/release/
