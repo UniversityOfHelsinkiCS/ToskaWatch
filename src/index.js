e7bb82e2d348159629747b2bcb4b3389800eef20
Mäkinen Sasu S <sasu.makinen@helsinki.fi>
2019-03-26 14:54:43 +0200

    change to dockerhub hook

--- src/index.js
+++ src/index.js
@@ -15,16 +15,12 @@ app.post('/', (req, res) => {
   const { payload } = req.body
   const data = JSON.parse(payload)
   console.log(data)
-  if (data.ref) {
-    const branch = data.ref.split('/')[2]
-    const commit_id = data.head_commit.id
-    const repository = data.repository.name
-    if (branch === 'master') {
-      const output = execSync(`./src/trigger-travis.sh --branch master UniversityOfHelsinkiCS ToskaWatch ${process.env.TRAVIS_ACCESS_TOKEN} "Tests triggered by a webhook from ${repository}-${commit_id}"`, { encoding: 'utf-8' })
-      console.log(output)
-    }
+  if (data.tag === 'staging') {
+    const output = execSync(`./src/trigger-travis.sh --branch master UniversityOfHelsinkiCS ToskaWatch ${process.env.TRAVIS_ACCESS_TOKEN} "Tests triggered by a webhook from ${repository}-${commit_id}"`, { encoding: 'utf-8' })
+    console.log(output)
   }
   res.status(200).end()
+
 })
 app.post('/release', (req, res) => {
   //auth here req.headers["Authorization"]
