496b23d62f19fb2fb4c0a5de29cd03f69ac669da
Kousa <jami.kousa@helsinki.fi>
2019-06-05 13:09:50 +0300

    pinger requires 2 failures and tells when working

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const runPinger = require('./src/pinger/index');
 const schedule = require('node-schedule');
 
-const job = schedule.scheduleJob('Pinger', '*/15 * * * *', (time) => {
-  console.log(`Ran jobs at ${time}`)
+const job = schedule.scheduleJob('Pinger', '*/10 * * * *', (time) => {
+  console.log(`Ran pinger at ${time}`)
   runPinger()  
 });
 
--- src/pinger/index.js
+++ src/pinger/index.js
@@ -1,6 +1,9 @@
 const cypress = require('cypress')
 const axios = require('axios')
 
+const status = {}
+const SLACK_AT_FAILURE_COUNT = 2
+
 const postToSlack = async (text = 'Toskawatch broke') => {
   if (!process.env.SLACK_HOOK) return;
 
@@ -11,24 +14,43 @@ const postToSlack = async (text = 'Toskawatch broke') => {
   }
 }
 
+const handleTestFailure = (testIdentifier) => {
+  if (!status[testIdentifier]) return status[testIdentifier] = 1;
+  const newStatus = ++status[testIdentifier]
+  if (newStatus == SLACK_AT_FAILURE_COUNT) {
+    postToSlack(`Noot noot! Toskawatch has failed twice in row for ${newStatus} :penguin:`)
+  }
+}
+
+const handleTestSuccess = (testIdentifier) => {
+  if (status[testIdentifier] >= SLACK_AT_FAILURE_COUNT) {
+    postToSlack(`Doot doot! ${newStatus} works! :penguin:`)
+  }
+  status[testIdentifier] = 0
+}
+
 const runTests = async (spec) => {
   const testStatus = await cypress.run({
     project: __dirname,
     spec
   })
 
-  if (testStatus.totalFailed || !testStatus.totalTests) {
-    testStatus.runs.forEach(run => {
-      if (run.stats.failures) {
-        postToSlack(`Noot noot! Toskawatch failed for ${run.spec.name}`)
-      }
-    })
-  }
+  testStatus.runs.forEach(run => {
+    if (run.stats.failures) {
+      handleTestFailure(run.spec.name)
+    } else {
+      handleTestSuccess(run.spec.name)
+    }
+  })
 }
 
 const runPinger = async () => {
-  await runTests(`${__dirname}/cypress/integration/grappa/*`)
-  await runTests(`${__dirname}/cypress/integration/oodikone/*`)
+  try {
+    await runTests(`${__dirname}/cypress/integration/grappa/*`)
+  } catch (e) { console.log('Failed to run tests for grappa') }
+  try {
+    await runTests(`${__dirname}/cypress/integration/oodikone/*`)
+  } catch (e) { console.log('Failed to run tests for oodikone') }
 }
 
 module.exports = runPinger;
