78ff70d89a60f47dcfe4f45d3e0713079522917e
Kousa <jami.kousa@helsinki.fi>
2019-06-27 14:18:37 +0300

    oodikone staging migrated

--- src/pinger/cypress/integration/oodikone/staging.js
+++ src/pinger/cypress/integration/oodikone/staging.js
@@ -1,10 +1,10 @@
 describe('Oodikone', () => {
   it('shows welcome to oodikone', () => {
-    cy.visit("https://oodikone.cs.helsinki.fi/staging")
+    cy.visit("https://oodikone-staging.cs.helsinki.fi/")
     cy.url().should('include', 'login.helsinki.fi')
 
-    cy.get('#username').type(Cypress.env('username'))
-    cy.get('#password').type(Cypress.env('password'))
+    cy.get('#username').type(Cypress.env('username_test'))
+    cy.get('#password').type(Cypress.env('password_test'))
 
     cy.contains('Login').click()
     cy.url().should('include', 'oodikone')
