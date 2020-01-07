d2ce754dd80b4f70b7970467ddfa253d288a8ce2
Joona Heikkilä <21111572+cxcorp@users.noreply.github.com>
2020-01-07 18:36:47 +0200

    Fix cypress commands not being loaded

---
+++ src/pinger/cypress/support/commands.js
@@ -0,0 +1,34 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add("login", (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('acceptShibboDisclosureIfShown', () => {
+    // keep test broken until reminder test works
+//   cy.get('body').then($body => {
+//     if ($body.find('h2:contains("Accepting the disclosure of your information")').length > 0) {
+//       cy.contains('button', 'Accept').click()
+//     }
+//   })
+})
