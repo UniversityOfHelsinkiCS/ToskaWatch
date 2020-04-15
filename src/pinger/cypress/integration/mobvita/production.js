75b15f8a34d9fb05dee97730c95f8e21233928f3
Marko Koskinen <markokoskinen2037@gmail.com>
2020-04-15 17:34:06 +0300

    test only login + story fetch

--- src/pinger/cypress/integration/mobvita/production.js
+++ src/pinger/cypress/integration/mobvita/production.js
@@ -3,46 +3,11 @@ describe('Mobvita', function () {
     cy.visit('https://mobvita.cs.helsinki.fi')
   })
 
-  it('can log in as anonymous', function () {
-    cy.reload()
-    cy.get('[data-cy=login-anon]')
-      .click()
-    cy.get('[data-cy=choose-lang]')
-  })
-
-  it('can log in as user', function () {
-    cy.reload(true)
-    cy.get('input:first')
-      .type('elbert.alyas@plutocow.com')
-    cy.get('input:last')
-      .type('emacsemacs')
-    cy.get('form')
-      .get('[data-cy=login]')
-      .click()
-    cy.get('[data-cy=practice-now]')
-  })
-
-  describe('when logged in', function () {
-    this.beforeEach(function () {
-      cy.request('POST', '/api/session', { email: 'elbert.alyas@plutocow.com', password: 'emacsemacs' })
-        .as('user')
-        .then(response => {
-          window.localStorage.setItem('user', JSON.stringify(response.body))
-          cy.reload()
-        })
-    })
-
-    it('library opens', function () {
-      cy.get('[href="/library"]')
-        .click()
-      cy.get('[data-cy=library-controls]')
-      cy.url().should('include', '/library')
-    })
-
-    it('can start random practice', function () {
-      cy.get('[data-cy=practice-now]').click()
-      cy.get('[data-cy=start-random]', { timeout: 10000 }).click()
-      cy.get('[data-cy=practice-view]')
-    })
+  it('can log in and stories are fetched corretly', function () {
+    cy.get('input:first').type('elbert.alyas@plutocow.com')
+    cy.get('input:last').type('emacsemacs')
+    cy.get('form').get('[data-cy=login]').click()
+    cy.get('[data-cy=practice-now]').click()
+    cy.get('[data-cy=start-random]').should('be.enabled')
   })
 })
