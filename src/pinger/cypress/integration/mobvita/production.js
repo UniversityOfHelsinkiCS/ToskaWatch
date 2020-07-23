8d2c1e8577feff81ba03c3ff8659f66d048f9ee9
Darake <adamson.gert@hotmail.com>
2020-07-23 12:56:29 +0300

    change mobvita prod login information

--- src/pinger/cypress/integration/mobvita/production.js
+++ src/pinger/cypress/integration/mobvita/production.js
@@ -4,8 +4,8 @@ describe('Mobvita', function () {
   })
 
   it('can log in and stories are fetched corretly', function () {
-    cy.get('input:first').type('elbert.alyas@plutocow.com')
-    cy.get('input:last').type('emacsemacs')
+    cy.get('input:first').type(Cypress.env('mobvita_email'))
+    cy.get('input:last').type(Cypress.env('mobvita_password'))
     cy.get('form').get('[data-cy=login]').click()
     cy.get('[data-cy=practice-now]').click()
     cy.get('[data-cy=start-random]').should('be.enabled')
