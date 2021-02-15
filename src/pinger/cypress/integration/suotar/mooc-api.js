4eec54bea790d589e0fc18cfa0e98de1c8a5c78b
Saara Satokangas <saarasat@gmail.com>
2021-02-15 09:33:49 +0200

    Add also content-checking to mooc-api healthcheck

--- src/pinger/cypress/integration/suotar/mooc-api.js
+++ src/pinger/cypress/integration/suotar/mooc-api.js
@@ -1,5 +1,5 @@
 describe('Mooc-api', () => {
-  it('is alive', () => {
+  it('is alive and giving data in correct form', () => {
     cy.request({
       url: Cypress.env('MOOC_API_URL'),
       headers: {
@@ -7,6 +7,11 @@ describe('Mooc-api', () => {
       } 
     }).then((response) => {
       expect(response.status).to.eq(200)
+      expect(response.body).to.have.length.gt(0)
+
+      const instance = response.body[0]
+      expect(instance).to.have.property('student_number')
+      expect(instance).to.have.property('id')
     })
   })
 })
