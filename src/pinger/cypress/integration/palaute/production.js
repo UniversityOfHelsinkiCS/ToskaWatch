5435f91cbed314e9a07379f8907d288be06d540f
LeoVaris <leovaris04@gmail.com>
2021-08-11 14:28:20 +0300

    add palaute

---
+++ src/pinger/cypress/integration/palaute/production.js
@@ -0,0 +1,14 @@
+describe('Fuksilaiterekisteri', () => {
+  it('shows page title', () => {
+    cy.visit('https://study.cs.helsinki.fi/palaute')
+    cy.url().should('include', 'login.helsinki.fi')
+
+    cy.get('#username').type(Cypress.env('username'))
+    cy.get('#password').type(Cypress.env('password'))
+
+    cy.contains('Login').click()
+    cy.acceptShibboDisclosureIfShown()
+
+    cy.contains('Norppa')
+  })
+})
